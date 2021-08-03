914893b88b0952c51c9fe0cd9b09f45e04d93774
Arun <arun.kumar4@publicissapient.com>
2021-08-03 11:16:55 +0530

    Swift UI Custom tabs

---
+++ SwiftUICustomTabs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICustomTabs
+//
+//  Created by Arun Jayasree Kumar on 03/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICustomTabs/SwiftUICustomTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomTabsApp.swift
+//  SwiftUICustomTabs
+//
+//  Created by Arun Jayasree Kumar on 03/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
